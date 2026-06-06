a57dceb89d53111e9b7125d6226d7075dc53cab5
adithyasureshb <989sure@gmail.com>
2026-06-06 09:42:21 +0530

    add postgresql support

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./study_planner.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+import os
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./study_planner.db")
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
